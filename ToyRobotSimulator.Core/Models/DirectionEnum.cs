e9eb2cb67864e40d6ee9c40fd062791248de86c2
yunzeli029 <liyunze029@gmail.com>
2019-10-27 13:45:17 +1100

    Add Enums(Command, Direction) and DirectionNode

---
+++ ToyRobotSimulator.Core/Models/DirectionEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotSimulator.Core.Models
+{
+    enum DirectionEnum
+    {
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+    }
+}
---
+++ ToyRobotSimulator.Core/Models/DirectionNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotSimulator.Core.Models
+{
+    public class DirectionNode<T>
+    {
+
+        public T current;
+        public DirectionNode<T> prev;
+        public DirectionNode<T> next;
+        public DirectionNode(T d)
+        {
+            current = d;
+            prev = null;
+            next = null;
+        }
+    }
+}
