785c951528f734057ffe4bd07c6758b41a5d577b
yunzeli029 <liyunze029@gmail.com>
2019-10-27 20:10:04 +1100

    Add Position and Boundary class

---
+++ ToyRobotSimulator.Core/Models/Boundary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotSimulator.Core.Models
+{
+    public class Boundary
+    {
+        public int MinX { get; set; }
+        public int MinY { get; set; }
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+        public Boundary(int minX, int minY, int maxX, int maxY)
+        {
+            this.MinX = minX;
+            this.MinY = minY;
+            this.MaxX = maxX;
+            this.MaxY = maxY;
+        }
+    }
+}
